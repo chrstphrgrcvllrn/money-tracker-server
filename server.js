d4f6d5ef386bda47855dd84baed5e7ff98e9a400
christopher-villarin <christopher.villarin@hogarth.com>
2026-03-23 15:58:43 +0800

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const billRoutes = require("./routes/billRoutes");
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
